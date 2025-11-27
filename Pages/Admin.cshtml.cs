4c0fb07de08eb905609361f64d9a57759e9ae186
sk2235 <sk2235@hw.ac.uk>
2025-11-27 21:54:49 +0400

    Secure admin page - require authentication to access

--- Pages/Admin.cshtml.cs
+++ Pages/Admin.cshtml.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WinInventory.Pages;
 
+[Authorize]
 public class AdminModel : PageModel
 {
     public void OnGet()
